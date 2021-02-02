08d61d32594f12e908738eda7ed1640db3a7f257
Alex's Macbook <themac@Alexs-MacBook-Pro.local>
2021-02-02 13:01:09 -0500

    updated component

--- src/FoodList.js
+++ src/FoodList.js
@@ -1,6 +1,60 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+function FoodList(props) {
+  const [beverage, setBeverage] = useState("");
+  const [food, setFood] = useState("");
+
+  const logName = () => {
+    console.log(beverage);
+    console.log(food);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(` Items You're Bringing : ${food} / ${beverage}`);
+  };
+
+  const handleBeverageInput = (e) => {
+    setBeverage(e.target.value);
+  };
+  const handleFoodInput = (e) => {
+    setFood(e.target.value);
+  };
+
+  return (
+    <FoodListDiv>
+      <H1>Food List</H1>
+      <form onSubmit={handleSubmit}>
+        <FoodLabel>
+          <label htmlFor="food">
+            <H2>Add Food:</H2>
+          </label>
+          <input
+            name="food"
+            placeholder="Food"
+            value={food}
+            onChange={handleFoodInput}
+          />
+        </FoodLabel>
+        <BeverageLabel>
+          <label htmlFor="beverage">
+            <H2> Add Beverage:</H2>
+          </label>
+          <input
+            name="beverage"
+            placeholder="Beverage"
+            value={beverage}
+            onChange={handleBeverageInput}
+          />
+        </BeverageLabel>
+        <SubmitDiv>
+          <Button onClick={logName}>Submit</Button>
+        </SubmitDiv>
+      </form>
+    </FoodListDiv>
+  );
+}
 // Styled Components
 const FoodListDiv = styled.div`
   background-color: #00af91;
@@ -45,64 +99,5 @@ const H1 = styled.h1`
 const H2 = styled.h2`
   color: #ffcc29;
 `;
-class FoodList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { food: "", beverage: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  handleSubmit(event) {
-    const { food, beverage } = this.state;
-    event.preventDefault();
-    alert(` 
-       Items You're Bringing\n 
-      Food : ${food} 
-      Beverage: ${beverage} 
-      
-    `);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  render() {
-    return (
-      <FoodListDiv>
-        <H1>Food List</H1>
-        <form onSubmit={this.handleSubmit}>
-          <FoodLabel>
-            <label htmlFor="food">
-              <H2>Add Food:</H2>
-            </label>
-            <input
-              name="food"
-              placeholder="Food"
-              value={this.state.food}
-              onChange={this.handleChange}
-            />
-          </FoodLabel>
-          <BeverageLabel>
-            <label htmlFor="beverage">
-              <H2> Add Beverage:</H2>
-            </label>
-            <input
-              name="beverage"
-              placeholder="Beverage"
-              value={this.state.beverage}
-              onChange={this.handleChange}
-            />
-          </BeverageLabel>
-          <SubmitDiv>
-            <Button>Submit</Button>
-          </SubmitDiv>
-        </form>
-      </FoodListDiv>
-    );
-  }
-}
 export default FoodList;
